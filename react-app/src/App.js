e3738af6beb7e9b2b8bb8343ecf219c547d0fcff
Xkushal <kushalyhakuri123@gmail.com>
2019-09-29 21:51:33 -0500

    react app

---
+++ react-app/src/App.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+import ClickBroken from './ClickBroken';
+import './App.css';
+
+class App extends Component {
+	render() {
+		return (
+			<div>
+				<h1>kushal</h1>
+				<h2>CSCI</h2>
+				<ClickBroken />
+			</div>
+		);
+	}
+}
+
+export default App;
---
+++ react-app/src/ClickBroken.js
@@ -0,0 +1,9 @@
+import React, { Component } from 'react';
+
+class ClickBroken extends Component {
+	render() {
+		return <h1>tutorial</h1>;
+	}
+}
+
+export default ClickBroken;
